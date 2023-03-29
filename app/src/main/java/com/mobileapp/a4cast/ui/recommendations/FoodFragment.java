d638e4ea6fca7ee28cc0ad0b9f4915a9bc2a866a
Robert Russell <112729862+Russell01876828@users.noreply.github.com>
2023-03-29 14:38:48 -0400

    added activity and food fragment lists

--- app/src/main/java/com/mobileapp/a4cast/ui/recommendations/FoodFragment.java
+++ app/src/main/java/com/mobileapp/a4cast/ui/recommendations/FoodFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,13 +18,22 @@ import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mobileapp.a4cast.DatabaseItem;
 import com.mobileapp.a4cast.GlobalData;
+import com.mobileapp.a4cast.ModelClass;
 import com.mobileapp.a4cast.R;
+import com.mobileapp.a4cast.RecyclerViewAdaptor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FoodFragment extends Fragment { // FOOD
     View view;
+    RecyclerViewAdaptor adapter;
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    List<ModelClass>displayList;
+    List<DatabaseItem> foodList;
+    List<DatabaseItem> conditions;
+    List<DatabaseItem> temps;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_food, container, false);
@@ -31,14 +42,13 @@ public class FoodFragment extends Fragment { // FOOD
         BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.nav_view);
         bottomNavigationView.setVisibility(View.GONE);
 
-        TextView text1 = view.findViewById(R.id.textView1);
 
         // get the conditions list from another fragment
-        List<DatabaseItem> conditions = GlobalData.getInstance().getConditions();
-        List<DatabaseItem> temps = GlobalData.getInstance().getTemps();
+        conditions = GlobalData.getInstance().getConditions();
+        temps = GlobalData.getInstance().getTemps();
         Log.d("DEBUG", "FOOD FRAGMENT: condition.size: " + conditions.size());
         Log.d("DEBUG", "FOOD FRAGMENT: temp.size: " + temps.size());
-        List<DatabaseItem> foodList = new ArrayList<>();
+        foodList = new ArrayList<>();
         //CREATE NEW LIST
         for (int i = 0; i < conditions.size(); i++) {
             DatabaseItem item1 = conditions.get(i);
@@ -58,12 +68,14 @@ public class FoodFragment extends Fragment { // FOOD
                 }
             }
         }
+        displayList = new ArrayList<>();
+        initData();
+        initRecyclerView();
 
-        String tempSTR = "";
-        for (int i = 0; i < foodList.size(); i++) {
-                tempSTR += foodList.get(i).getName() + "\n";
-        }
-        text1.setText(tempSTR);
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        //adapter = new RecyclerViewAdaptor(this, colorsNames);
+        recyclerView.setAdapter(adapter);
 
         /**
          * Example:
@@ -88,5 +100,103 @@ public class FoodFragment extends Fragment { // FOOD
         Log.d("DEBUG", "FOOD FRAGMENT: ON_DESTROY");
         super.onDestroyView();
 
+    }
+
+    private void initData(){ //CHANGE HERE
+        for(int i = 0; i < foodList.size(); i++) {
+            DatabaseItem dbItem = foodList.get(i);
+            switch (dbItem.getName()) {
+                case "LEGGINGS":
+                    displayList.add(new ModelClass(R.drawable.leggings, "Leggings", dbItem.getLink()));
+                    break;
+                case "EARMUFFS":
+                    displayList.add(new ModelClass(R.drawable.earmuffs, "Earmuffs", dbItem.getLink()));
+                    break;
+                case "GLOVES":
+                    displayList.add(new ModelClass(R.drawable.gloves, "Gloves", dbItem.getLink()));
+                    break;
+                case "WOOLEN HAT":
+                    displayList.add(new ModelClass(R.drawable.woolenhat, "Woolen Hat", dbItem.getLink()));
+                    break;
+                case "SWEATER":
+                    displayList.add(new ModelClass(R.drawable.sweater, "Sweater", dbItem.getLink()));
+                    break;
+                case "PADDED JACKET":
+                    displayList.add(new ModelClass(R.drawable.paddedjacket, "Padded Jacket", dbItem.getLink()));
+                    break;
+                case "FLEECE-LINED PANTS":
+                    displayList.add(new ModelClass(R.drawable.fleecelinedpants, "Fleece-lined Pants", dbItem.getLink()));
+                    break;
+                case "LONG UNDERWEAR":
+                    displayList.add(new ModelClass(R.drawable.longunderwear, "Long Underwear", dbItem.getLink()));
+                    break;
+                case "COAT":
+                    displayList.add(new ModelClass(R.drawable.coat, "Coat", dbItem.getLink()));
+                    break;
+                case "BOOTS":
+                    displayList.add(new ModelClass(R.drawable.boots, "Boots", dbItem.getLink()));
+                    break;
+                case "JEANS":
+                    displayList.add(new ModelClass(R.drawable.jeans, "Jeans", dbItem.getLink()));
+                    break;
+                case "TRENCH COAT":
+                    displayList.add(new ModelClass(R.drawable.trenchcoat, "Trench Coat", dbItem.getLink()));
+                    break;
+                case "HOOD T-SHIRT":
+                    displayList.add(new ModelClass(R.drawable.hoodtshirt, "Hood T-Shirt", dbItem.getLink()));
+                    break;
+                case "SNEAKERS":
+                    displayList.add(new ModelClass(R.drawable.sneakers, "Sneakers", dbItem.getLink()));
+                    break;
+                case "LOAFERS":
+                    displayList.add(new ModelClass(R.drawable.loafers, "Loafer", dbItem.getLink()));
+                    break;
+                case "LEATHER JACKET":
+                    displayList.add(new ModelClass(R.drawable.leatherjacket, "Leather Jacket", dbItem.getLink()));
+                    break;
+                case "CHINO PANTS":
+                    displayList.add(new ModelClass(R.drawable.chinopants, "Chino Pants", dbItem.getLink()));
+                    break;
+                case "T-SHIRTS":
+                    displayList.add(new ModelClass(R.drawable.tshirts, "T-Shirts", dbItem.getLink()));
+                    break;
+                case "JACKET":
+                    displayList.add(new ModelClass(R.drawable.jacket, "Jacket", dbItem.getLink()));
+                    break;
+                case "CARDIGAN":
+                    displayList.add(new ModelClass(R.drawable.cardigan, "Cardigan", dbItem.getLink()));
+                    break;
+                case "DRESS SHIRTS":
+                    displayList.add(new ModelClass(R.drawable.dressshirts, "Dress Shirts", dbItem.getLink()));
+                    break;
+                case "SUNGLASSES":
+                    displayList.add(new ModelClass(R.drawable.sunglasses, "Sunglasses", dbItem.getLink()));
+                    break;
+                case "SLEEVELESS":
+                    displayList.add(new ModelClass(R.drawable.sleeveless, "Sleeveless", dbItem.getLink()));
+                    break;
+                case "SHORTS":
+                    displayList.add(new ModelClass(R.drawable.shorts, "Shorts", dbItem.getLink()));
+                    break;
+                case "ONE PIECE":
+                    displayList.add(new ModelClass(R.drawable.onepiece, "One Piece", dbItem.getLink()));
+                    break;
+                case "SANDAL":
+                    displayList.add(new ModelClass(R.drawable.sandal, "Sandals", dbItem.getLink()));
+                    break;
+                default:
+                    displayList.add(new ModelClass(R.drawable.sun, "DEFAULT", dbItem.getLink()));
+                    break;
+            }
+        }
+    }
+
+    private void initRecyclerView() {
+        recyclerView=view.findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(view.getContext());
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerViewAdaptor(displayList);
+        adapter.notifyDataSetChanged();
     }
 }
