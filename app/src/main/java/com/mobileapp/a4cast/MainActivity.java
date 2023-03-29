881f267075858a58bfeeffb298e822fcf46e14c7
Robert Russell <112729862+Russell01876828@users.noreply.github.com>
2023-03-29 13:58:32 -0400

    Testing

--- app/src/main/java/com/mobileapp/a4cast/MainActivity.java
+++ app/src/main/java/com/mobileapp/a4cast/MainActivity.java
@@ -15,7 +15,16 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mobileapp.a4cast.databinding.ActivityMainBinding;
+/*
+Mobile App Development II -- COMP.4631 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order in the classroom, providing an enriching learning experience for students, and training as a practicing computing professional upon graduation. This practice is manifested in the University's Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be dealt with as outlined therein. All programming assignments in this class are to be done by the student alone unless otherwise specified. No outside help is permitted except the instructor and approved tutors.
 
+I certify that the work submitted with this assignment is mine and was generated in a manner consistent with this document, the course academic policy on the course website on Blackboard, and the UMass Lowell academic code.
+
+Date: 28/03/2023
+Name: Dongjun Gu, Robert Russell
+https://github.com/UML-COMP4630-LIN/project-prerelease-group9_dongjungu_robertrussell
+*/
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -52,12 +61,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-/*
-Mobile App Development II -- COMP.4631 Honor Statement
-The practice of good ethical behavior is essential for maintaining good order in the classroom, providing an enriching learning experience for students, and training as a practicing computing professional upon graduation. This practice is manifested in the University's Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be dealt with as outlined therein. All programming assignments in this class are to be done by the student alone unless otherwise specified. No outside help is permitted except the instructor and approved tutors.
-
-I certify that the work submitted with this assignment is mine and was generated in a manner consistent with this document, the course academic policy on the course website on Blackboard, and the UMass Lowell academic code.
-
-Date: 28/03/2023
-Name: Dongjun Gu, Robert Russell
-*/
